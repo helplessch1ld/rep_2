b37fd621d9838e38458f1d4430d873a0a091c26c
helplessch1ld <bvdshzr881@gmail.com>
2026-04-03 01:23:09 +0700

    Add shuttle_sort.py

---
+++ shuttle_sort.py
@@ -0,0 +1,24 @@
+﻿import random 
+def shuttle_sort(arr, ascending=True):    
+    n = len(arr)
+    result = arr.copy()
+    
+    for i in range(1, n):
+        j = i
+        while j > 0:
+            if ascending:
+                if result[j] < result[j - 1]:
+                    result[j], result[j - 1] = result[j - 1], result[j]
+                    j -= 1
+                else:
+                    break
+            else:
+                if result[j] > result[j - 1]:
+                    result[j], result[j - 1] = result[j - 1], result[j]
+                    j -= 1
+                else:
+                    break
+    
+    return result
+
+
