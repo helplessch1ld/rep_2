492aaf1c57fd10ab256dd960defa69b0a786dcdc
helplessch1ld <bvdshzr881@gmail.com>
2026-04-03 01:15:52 +0700

    Add sorting without array

---
+++ linear_sorting_no_arr.py
@@ -0,0 +1,19 @@
+﻿def linear_sort_no_arr(arr, ascending=True):
+    n = len(arr)
+    result = arr.copy()
+    
+    for i in range(n):
+        extremum_idx = i
+        for j in range(i + 1, n):
+            if ascending:
+                if result[j] < result[extremum_idx]:
+                    extremum_idx = j
+            else:
+                if result[j] > result[extremum_idx]:
+                    extremum_idx = j
+        
+        if extremum_idx != i:
+            result[i], result[extremum_idx] = result[extremum_idx], result[i]
+    
+    return result
+
