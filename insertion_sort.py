cee32cfc26cbc49f98ce66d4ea9c74a850cf712a
helplessch1ld <bvdshzr881@gmail.com>
2026-04-03 01:28:14 +0700

    Add insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,21 @@
+﻿def insertion_sort(arr, ascending=True):
+    n = len(arr)
+    result = arr.copy()
+    
+    for i in range(1, n):
+        key = result[i]
+        j = i - 1
+        
+        if ascending:
+            while j >= 0 and result[j] > key:
+                result[j + 1] = result[j]
+                j -= 1
+        else:
+            while j >= 0 and result[j] < key:
+                result[j + 1] = result[j]
+                j -= 1
+        
+        result[j + 1] = key
+    
+    return result
+
